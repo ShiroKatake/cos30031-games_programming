44e1f9cf76df06aa176a08aa1ef9e800c8a3b8ba
ShiroKatake <tgkhang262@gmail.com>
2020-09-28 13:51:56 +1000

    UPDATE: Task_03, function to move the player

--- 03 - Spike - Gridworld/Task_03/Task_03.cpp
+++ 03 - Spike - Gridworld/Task_03/Task_03.cpp
@@ -19,8 +19,24 @@ char keyboardInput;
 int x = 2;
 int y = 7;
 
-void ProcessInput() {
+bool win = false;
+bool lose = false;
 
+void Move(int horizontal, int vertical) {
+	int x2 = x + horizontal;
+	int y2 = y + vertical;
+	if (map[y2][x2] != 1) {
+		map[y][x] = 0;
+		x += horizontal;
+		y += vertical;
+	}
+	if (map[y2][x2] == 3) {
+		win = true;
+	}
+	if (map[y2][x2] == 4) {
+		lose = true;
+	}
+	map[y][x] = 2;
 }
 
 void Update() {

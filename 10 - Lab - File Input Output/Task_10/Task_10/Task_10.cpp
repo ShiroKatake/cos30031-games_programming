e63113601747e50864a0a222facf9c98908b97e4
ShiroKatake <tgkhang262@gmail.com>
2020-10-22 14:20:03 +1100

    UPDATE #10.4.0: Reading binary file code

--- 10 - Lab - File Input Output/Task_10/Task_10/Task_10.cpp
+++ 10 - Lab - File Input Output/Task_10/Task_10/Task_10.cpp
@@ -18,12 +18,26 @@ public:
 int main() {
 
 	MyClass* myClass = new MyClass('asdf', 10, 3.2f);
-
-	ofstream testFile1("testFile1.bin", ios::binary);
-	testFile1.write(&myClass->myChar, sizeof(myClass->myChar));
-	testFile1.write(reinterpret_cast<const char*>(&myClass->myInt), sizeof(myClass->myInt));
-	testFile1.write(reinterpret_cast<const char*>(&myClass->myFloat), sizeof(myClass->myFloat));
-	testFile1.close();
+/*
+	ofstream writeTest1("testFile1.bin", ios::binary);
+	writeTest1.write(&myClass->myChar, sizeof(myClass->myChar));
+	writeTest1.write(reinterpret_cast<const char*>(&myClass->myInt), sizeof(myClass->myInt));
+	writeTest1.write(reinterpret_cast<const char*>(&myClass->myFloat), sizeof(myClass->myFloat));
+	writeTest1.close();*/
 	
+	char myCharRead = NULL;
+	int myIntRead = NULL;
+	float myFloatRead= NULL;
+
+	ifstream readTest1("testFile1.bin", ios::in | ios::binary);
+	readTest1.read(&myCharRead, sizeof(myClass->myChar));
+	readTest1.read(reinterpret_cast<char *>(&myIntRead), sizeof(myClass->myInt));
+	readTest1.read(reinterpret_cast<char *>(&myFloatRead), sizeof(myClass->myFloat));
+	readTest1.close();
+
+	cout << "char: " << myCharRead << endl;
+	cout << "int: " << myIntRead << endl;
+	cout << "float: " << myFloatRead << endl;
+
 	return 0;
 }

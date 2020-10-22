c7a82372b424c730eb37097cac5313a528f9c864
ShiroKatake <tgkhang262@gmail.com>
2020-10-22 14:46:38 +1100

    UPDATE #10.13.0: Disabled main in A script to do main in B script

--- 10 - Lab - File Input Output/Task_10/Task_10/Task_10A.cpp
+++ 10 - Lab - File Input Output/Task_10/Task_10/Task_10A.cpp
@@ -15,31 +15,31 @@ public:
 	float myFloat;
 };
 
-int main() {
-
-	MyClass* myClass = new MyClass('f', 10, 3.2f);
-	
-	/*
-	ofstream writeTest1("testFile1.bin", ios:out | ios::binary);
-	writeTest1.write(&myClass->myChar, sizeof(myClass->myChar));
-	writeTest1.write(reinterpret_cast<const char*>(&myClass->myInt), sizeof(myClass->myInt));
-	writeTest1.write(reinterpret_cast<const char*>(&myClass->myFloat), sizeof(myClass->myFloat));
-	writeTest1.close();
-	*/
-	
-	char myCharRead = NULL;
-	int myIntRead = NULL;
-	float myFloatRead= NULL;
-
-	ifstream readTest1("testFile1.bin", ios::in | ios::binary);
-	readTest1.read(&myCharRead, sizeof(myClass->myChar));
-	readTest1.read(reinterpret_cast<char *>(&myIntRead), sizeof(myClass->myInt));
-	readTest1.read(reinterpret_cast<char *>(&myFloatRead), sizeof(myClass->myFloat));
-	readTest1.close();
-
-	cout << "char: " << myCharRead << endl;
-	cout << "int: " << myIntRead << endl;
-	cout << "float: " << myFloatRead << endl;
-
-	return 0;
-}
+//int main() {
+//
+//	MyClass* myClass = new MyClass('f', 10, 3.2f);
+//	
+//	/*
+//	ofstream writeTest1("testFile1.bin", ios:out | ios::binary);
+//	writeTest1.write(&myClass->myChar, sizeof(myClass->myChar));
+//	writeTest1.write(reinterpret_cast<const char*>(&myClass->myInt), sizeof(myClass->myInt));
+//	writeTest1.write(reinterpret_cast<const char*>(&myClass->myFloat), sizeof(myClass->myFloat));
+//	writeTest1.close();
+//	*/
+//	
+//	char myCharRead = NULL;
+//	int myIntRead = NULL;
+//	float myFloatRead= NULL;
+//
+//	ifstream readTest1("testFile1.bin", ios::in | ios::binary);
+//	readTest1.read(&myCharRead, sizeof(myClass->myChar));
+//	readTest1.read(reinterpret_cast<char *>(&myIntRead), sizeof(myClass->myInt));
+//	readTest1.read(reinterpret_cast<char *>(&myFloatRead), sizeof(myClass->myFloat));
+//	readTest1.close();
+//
+//	cout << "char: " << myCharRead << endl;
+//	cout << "int: " << myIntRead << endl;
+//	cout << "float: " << myFloatRead << endl;
+//
+//	return 0;
+//}

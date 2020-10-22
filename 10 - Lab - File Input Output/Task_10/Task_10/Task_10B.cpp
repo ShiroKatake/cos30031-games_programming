2cf868f9f5486a02049043b05b71847453509ee6
ShiroKatake <tgkhang262@gmail.com>
2020-10-22 15:33:35 +1100

    UPDATE #10.17.0: Ignore lines as described by task

--- 10 - Lab - File Input Output/Task_10/Task_10/Task_10B.cpp
+++ 10 - Lab - File Input Output/Task_10/Task_10/Task_10B.cpp
@@ -7,13 +7,15 @@ int main() {
 	//Code learned from http://www.cplusplus.com/doc/tutorial/files/
 	string line;
 
-	ifstream readTest2("testFile2.txt", ios::in | ios::binary);
-	if (readTest2.is_open()) {
-		while (getline(readTest2, line)) {
+	ifstream readTest2("testFile2.txt", ios::in);
+	while (!readTest2.eof()) {
+		getline(readTest2, line);
+		
+		if (line.length() != 0 && line[0] != '#') {
 			cout << line << endl;
 		}
-		readTest2.close();
 	}
+	readTest2.close();
 
 	return 0;
 }

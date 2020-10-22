64b4acc7200bbc68d69f21aa467fc44a6fcb0aa9
ShiroKatake <tgkhang262@gmail.com>
2020-10-22 17:46:50 +1100

    UPDATE #10.26.0: Cleaned up excess code and added function for reading and output

--- 10 - Lab - File Input Output/Task_10/Task_10/Task_10C.cpp
+++ 10 - Lab - File Input Output/Task_10/Task_10/Task_10C.cpp
@@ -3,32 +3,20 @@
 #include <string>
 #include "json.hpp"
 using namespace std;
-
-void SeparateAndPrin(string& line) {
-	//Code learned from https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c
-	string delimiter = ":";
-
-	size_t pos = 0;
-	string token;
-	while ((pos = line.find(delimiter)) != string::npos) {
-		token = line.substr(0, pos);
-		cout << token << endl;
-		line.erase(0, pos + delimiter.length());
-	}
-	cout << line << endl;
-}
+using json = nlohmann::json;
 
 int main() {
 	string line;
 
 	ifstream readTest3("testFile3.json", ios::in);
-	while (!readTest3.eof()) {
-		getline(readTest3, line);
-
-		if (line.length() != 0 && line[0] != '#') {
-			cout << line << endl;
-		}
-	}
+	json j;
+	readTest3 >> j;
+	cout << j["jsonType"] << endl;
+	cout << j["name"] << endl;
+	cout << j["uuid"] << endl;
+	cout << j["health"] << endl;
+	cout << j["exp"] << endl;
+	cout << j["level"] << endl;
 
 	readTest3.close();
 

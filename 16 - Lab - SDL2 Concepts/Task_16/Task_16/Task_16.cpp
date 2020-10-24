e891eac768d4037b64c73ece046fa64a2fffb101
ShiroKatake <tgkhang262@gmail.com>
2020-10-24 14:35:04 +1100

    UPDATE #16.9.0: Added quit function, changed input for quit function from text command to button

--- 16 - Lab - SDL2 Concepts/Task_16/Task_16/Task_16.cpp
+++ 16 - Lab - SDL2 Concepts/Task_16/Task_16/Task_16.cpp
@@ -17,34 +17,38 @@ int main(int argg, char *argv[]) {
 	SDL_Event ev;
 
 	string text = "";
-	SDL_StartTextInput();
 
 	while (isRunning) {
 		while (SDL_PollEvent(&ev) != 0) {
 			if (ev.type == SDL_QUIT)
 				isRunning = false;
 			else if (ev.type == SDL_KEYDOWN) {
-				//If r is pressed, get a random color and clear > display that color on the screen
-				if (ev.key.keysym.sym == SDLK_r) {
-					SDL_SetRenderDrawColor(renderer, rand() % 255 + 1, rand() % 255 + 1, rand() % 255 + 1, rand() % 255 + 1);
-					SDL_RenderClear(renderer);
-					SDL_RenderPresent(renderer);
+				switch (ev.key.keysym.sym)
+				{
+					//If r is pressed, get a random color and clear > display that color on the screen
+					case SDLK_r:
+					{
+						SDL_SetRenderDrawColor(renderer, rand() % 255 + 1, rand() % 255 + 1, rand() % 255 + 1, rand() % 255 + 1);
+						SDL_RenderClear(renderer);
+						SDL_RenderPresent(renderer);
+						break;
+					}
+
+					//If q is pressed, exit
+					case SDLK_q:
+						isRunning = false;
+						break;
+
+					default:
+						break;
 				}
-
-				else if (ev.key.keysym.sym == SDLK_BACKSPACE && text.length() > 0)
-					text = text.substr(0, text.length() - 1);
-			}
-			else if (ev.type == SDL_TEXTINPUT)
-				system("cls");
-				text += ev.text.text;
-
-			cout << text << endl;
-			
+			}			
 		}
 		SDL_UpdateWindowSurface(window);
 	}
-	SDL_StopTextInput();
+
 	window = nullptr;
 	SDL_Quit();
+
 	return 0;
 }

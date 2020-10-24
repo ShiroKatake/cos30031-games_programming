4e5061add7264872b1abb66094675ba4ad045222
ShiroKatake <tgkhang262@gmail.com>
2020-10-24 14:38:46 +1100

    UPDATE #16.11.0: Window's title

--- 16 - Lab - SDL2 Concepts/Task_16/Task_16/Task_16.cpp
+++ 16 - Lab - SDL2 Concepts/Task_16/Task_16/Task_16.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main(int argg, char *argv[]) {
 	SDL_Init(SDL_INIT_VIDEO);
-	SDL_Window *window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+	SDL_Window *window = SDL_CreateWindow("Task 16", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
 
 	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);

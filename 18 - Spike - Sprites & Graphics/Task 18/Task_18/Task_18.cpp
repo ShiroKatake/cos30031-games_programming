bf361db07a0723f42eb35deb8c4344817bca53da
ShiroKatake <tgkhang262@gmail.com>
2020-10-28 18:01:10 +1100

    ADD #18.1.0: Initial cpp commit

---
+++ 18 - Spike - Sprites & Graphics/Task 18/Task_18/Task_18.cpp
@@ -0,0 +1,60 @@
+#include <string>
+#include <iostream>
+#include "SDL.h"
+
+using namespace std;
+
+int main(int argg, char *argv[]) {
+	SDL_Window *window = nullptr;
+	SDL_Surface *windowSurface = nullptr;
+	SDL_Surface *image1 = nullptr;
+	SDL_Surface *image2 = nullptr;
+	SDL_Surface *image3 = nullptr;
+	SDL_Init(SDL_INIT_VIDEO);
+	window = SDL_CreateWindow("Task 16", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+
+	bool isRunning = true;
+	SDL_Event ev;
+
+	while (isRunning) {
+		while (SDL_PollEvent(&ev) != 0) {
+			if (ev.type == SDL_QUIT)
+				isRunning = false;
+			else if (ev.type == SDL_KEYDOWN) {
+				switch (ev.key.keysym.sym)
+				{
+					//If r is pressed, get a random color and clear > display that color on the screen
+					case SDLK_r:
+					{
+						SDL_SetRenderDrawColor(renderer, rand() % 255 + 1,
+							rand() % 255 + 1,
+							rand() % 255 + 1,
+							rand() % 255 + 1);
+						SDL_RenderClear(renderer);
+						SDL_RenderPresent(renderer);
+						break;
+					}
+
+					//If q is pressed, exit
+					case SDLK_q:
+						isRunning = false;
+						break;
+
+					default:
+						break;
+				}
+			}
+		}
+		SDL_UpdateWindowSurface(window);
+	}
+
+	window = nullptr;
+	SDL_Quit();
+
+	return 0;
+}

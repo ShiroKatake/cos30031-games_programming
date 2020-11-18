5b5c8d6f977c92a659c5bc89d778a6db44a4df56
ShiroKatake <tgkhang262@gmail.com>
2020-11-19 01:37:42 +1100

    UPDATE #21.6.0: Learned about "inline" to fix the issue

--- 21 - Spike - Announcements & Blackboards/Task_21/Task_21.cpp
+++ 21 - Spike - Announcements & Blackboards/Task_21/Task_21.cpp
@@ -19,7 +19,7 @@ public:
 		this->receiverId = receiverId;
 	}
 
-	std::string GetEvent()
+	std::string GetMessage()
 	{
 		return messageEvent;
 	}
@@ -29,190 +29,65 @@ public:
 	}
 };
 
-class MessageBus {
+class Receiver;
+
+class Mailbox {
 private:
-	std::vector<std::function<void(Message)>> receivers;
-	std::queue<Message> messages;
+	vector<Message> messages;
+	vector<Receiver*> receivers;
 
 public:
-	MessageBus() {};
-	~MessageBus() {};
+	void AddReceiver(Receiver* receiver) {
+		receivers.push_back(receiver);
+	}
+	void StoreMessage(Message message);
+	void Deliver();
+};
 
-	void AddReceiver(std::function<void(Message)> messageReceiver)
-	{
-		receivers.push_back(messageReceiver);
+class Receiver {
+private:
+	string name;
+	int uid;
+	Mailbox* mailbox;
+public:
+	Receiver(string name, int uid, Mailbox* mailbox) {
+		this->name = name;
+		this->uid = uid;
+		this->mailbox = mailbox;
+		mailbox->AddReceiver(this);
 	}
 
-	void SendMessage(Message message)
-	{
-		messages.push(message);
+	void CreateMessage(string event, int id) {
+		Message message(event, id);
+		mailbox->StoreMessage(message);
 	}
 
-	void Notify()
-	{
-		while (!messages.empty()) {
-			for (std::size_t i = 0; i < receivers.size(); ++i) {
-				if (messages.front().IsMessageForReceiver(i)) {
-					messages.front();
-				}
-			}
-
-			/*for (auto iter = receivers.begin(); iter != receivers.end(); iter++) {
-				(*iter)(messages.front());
-			}*/
-
-			messages.pop();
+	void ReceiveMessage(Message message) {
+		if (message.IsMessageForReceiver(uid)) {
+			cout << name << " received: " << message.GetMessage() << endl;
 		}
 	}
 };
 
-class BusNode {
-protected:
-	MessageBus *messageBus;
+inline void Mailbox::StoreMessage(Message message) {
+	messages.push_back(message);
+}
 
-	std::function<void(Message)> getNotifyFunc()
-	{
-		auto messageListener = [=](Message message) -> void {
-			this->onNotify(message);
-		};
-		return messageListener;
+inline void Mailbox::Deliver() {
+	for (Receiver* receiver : receivers) {
+		for (Message message : messages) {
+			receiver->ReceiveMessage(message);
+		}
 	}
-
-	void Send(Message message)
-	{
-		messageBus->SendMessage(message);
-	}
-
-	virtual void onNotify(Message message)
-	{
-		// Do something here. Your choice. But you could do this.
-		// std::cout << "Siopao! Siopao! Siopao! (Someone forgot to implement onNotify().)" << std::endl;
-	}
-
-public:
-	BusNode(MessageBus *messageBus)
-	{
-		this->messageBus = messageBus;
-		this->messageBus->AddReceiver(this->getNotifyFunc());
-	}
-
-	virtual void Update() {}
-};
-
-// This class will receive a message from ComponentB.
-class ComponentA : public BusNode {
-public:
-	ComponentA(MessageBus* messageBus) : BusNode(messageBus) {}
-
-private:
-	void onNotify(Message message)
-	{
-		std::cout << "Component A received: " << message.GetEvent() << std::endl;
-	}
-};
-
-// This class will send a message to ComponentA.
-class ComponentB : public BusNode {
-public:
-	ComponentB(MessageBus* messageBus) : BusNode(messageBus) {}
-
-	void update()
-	{
-		Message greeting("Hi!", 0);
-		Send(greeting);
-	}
-
-private:
-	void onNotify(Message message)
-	{
-		std::cout << "Component B received: " << message.GetEvent() << std::endl;
-	}
-};
-
-//class Message2 {
-//private:
-//	std::string messageEvent;
-//	int receiverId;
-//
-//public:
-//	Message2(const std::string event, const int receiverId)
-//	{
-//		messageEvent = event;
-//		this->receiverId = receiverId;
-//	}
-//
-//	std::string GetMessage()
-//	{
-//		return messageEvent;
-//	}
-//
-//	bool IsMessageForReceiver(int id) {
-//		return receiverId == id;
-//	}
-//};
-//
-//class Mailbox {
-//private:
-//	vector<Message2> messages;
-//	vector<std::function<void(Message)>> receivers;
-//public:
-//	void AddReceiver(Receipient receipient) {
-//		receivers.push_back(receipient);
-//	}
-//	void CreateMessage(Message2 message) {
-//		messages.push_back(message);
-//	}
-//
-//	void Deliver() {
-//		for (std::size_t i = 0; i < receivers.size(); ++i) {
-//			for (Message2 m : messages) {
-//				if (m.IsMessageForReceiver(i))
-//					receivers[i].ReceiveMessage(m);
-//			}
-//		}
-//	}
-//};
-//
-//class Receipient {
-//public:
-//	int uid;
-//
-//	Receipient(int uid, Mailbox* mailbox) {
-//		this->uid = uid;
-//		mailbox->AddReceiver(*this);
-//	}
-//
-//	void CreateMessage(string event, int id, Mailbox* mailbox) {
-//		Message2 message(event, id);
-//		mailbox->CreateMessage(message);
-//	}
-//
-//	void ReceiveMessage(Message2 message) {
-//		cout << "Component " << this << "received: " << message.GetMessage() << endl;
-//	}
-//};
+}
 
 int main() {
-	//Mailbox mailbox;
-	//Receipient compA(1, &mailbox);
-	//Receipient compB(2, &mailbox);
-	//
-	//compB.CreateMessage("Hi!", 1, &mailbox);
-	//mailbox.Deliver();
+	Mailbox mailbox;
+	Receiver compA("component A", 1, &mailbox);
+	Receiver compB("component B", 2, &mailbox);
 
-	MessageBus messageBus;
-
-	//Creating ComponentA first will put it at the id of 0
-	ComponentA compA(&messageBus);
-	ComponentB compB(&messageBus);
-
-	// This is supposed to act like a game loop.
-	for (int ctr = 0; ctr < 2; ctr++) {
-		compA.Update();
-		compB.update();
-		messageBus.Notify();
-	}
-
+	compB.CreateMessage("Hi!", 1);
+	mailbox.Deliver();
 	std::cin.get();
 
 	return 0;
